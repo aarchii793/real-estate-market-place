557fe7eacc8980f41aafc25f5bf5e85d6afd2cd1
aarchii793 <prajapatiaarchi15@gmail.com>
2025-06-20 00:26:03 +0530

    update about and title name

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -28,7 +28,7 @@ export default function Header() {
         <Link to='/'>
           <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
             <span className='text-slate-500'>Brick &</span>
-            <span className='text-slate-700'>Beam</span>
+            <span className='text-slate-700'> Beam</span>
           </h1>
         </Link>
         <form
